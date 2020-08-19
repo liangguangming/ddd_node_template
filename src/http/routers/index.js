ce6f51d66c71e7c65ce08edfce30d7634ee2d1c2
liangguangming <gmliangd@163.com>
2020-08-19 16:55:34 +0800

    添加应用状态获取接口

--- src/http/routers/index.js
+++ src/http/routers/index.js
@@ -1,9 +1,17 @@
-const Router = require('koa-router');
+import Router from 'koa-router';
+
+import app from '../../app';
+import Result from '../../share/core/result';
 
 const home = new Router();
 
 home.get('/', (ctx) => {
   ctx.body = '首页';
+});
+
+home.get('/status', (ctx) => {
+  const status = app.getStatus();
+  ctx.body = new Result(null, '', status);
 });
 
 module.exports = home.routes();
--- src/http/routers/router.js
+++ src/http/routers/router.js
@@ -4,7 +4,7 @@ const user = require('./user');
 
 const router = new Router();
 
-router.use('/', home);
+router.use('', home);
 router.use('/users', user);
 
 module.exports = router;
